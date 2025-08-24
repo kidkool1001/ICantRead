f26b8fcb79e904f504ef8fc495a284e17b2f8ae4
kidkool1001 <kidkool1001@proton.me>
2025-08-24 15:16:31 -0400

    Add project files.

---
+++ ICantRead/Program.cs
@@ -0,0 +1,71 @@
+﻿using BepInEx;
+using TMPro;
+using UnityEngine;
+using System;
+using System.Text.RegularExpressions;
+using System.Globalization;
+using System.Reflection;
+using EFT.UI.Matchmaker;
+using SPT.Reflection.Patching;
+
+namespace ICantRead
+{
+    [BepInPlugin("com.kidkool1001.icantread", "I Can't Read", "1.0.0")]
+    public class ICantReadPlugin : BaseUnityPlugin
+    {
+        private void Awake()
+        {
+            new TimePatch().Enable();
+            Logger.LogInfo("Embracing Illiteracy.. (I Can't Read Loaded)");
+        }
+    }
+
+    public class TimePatch : ModulePatch
+    {
+        protected override MethodBase GetTargetMethod()
+        {
+            return typeof(LocationConditionsPanel).GetMethod("Set", BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic);
+        }
+
+        [PatchPostfix]
+        private static void PatchPostfix(LocationConditionsPanel __instance)
+        {
+            if (__instance == null) return;
+
+            if (__instance.gameObject.GetComponent<TimeLabelUpdater>() == null)
+            {
+                __instance.gameObject.AddComponent<TimeLabelUpdater>();
+            }
+        }
+    }
+
+    public class TimeLabelUpdater : MonoBehaviour
+    {
+        private TMP_Text[] _labels;
+        private static readonly Regex MSpaceRegex = new Regex("<.*?>", RegexOptions.Compiled);
+
+        private void Awake()
+        {
+            _labels = GetComponentsInChildren<TMP_Text>(true);
+        }
+
+        private void LateUpdate()
+        {
+            foreach (var label in _labels)
+            {
+                if (string.IsNullOrEmpty(label.text)) continue;
+
+                string clean = MSpaceRegex.Replace(label.text, "");
+
+                if (DateTime.TryParseExact(clean,
+                    new[] { "HH:mm:ss", "HH:mm" },
+                    CultureInfo.InvariantCulture,
+                    DateTimeStyles.None,
+                    out DateTime dt))
+                {
+                    label.text = dt.ToString("hh:mm:ss") + $"<line-height=0.7><size=40%>{dt.ToString("tt")}</size></line-height>";
+                }
+            }
+        }
+    }
+}
